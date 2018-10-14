acfdcc8c728e1962d7a3212b6bfc408c40ba9e0d
大石头 <nnhy@vip.qq.com>
2018-10-14 11:03:44 +0800

    增加文字转语音助手

---
+++ XCoder/Tools/FrmSpeak.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace XCoder.Tools
+{
+    partial class FrmSpeak
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(719, 426);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "学无先后达者为师！";
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSpeak.Location = new System.Drawing.Point(737, 12);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(51, 133);
+            this.btnSpeak.TabIndex = 1;
+            this.btnSpeak.Text = "文字转语音";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            // 
+            // FrmSpeak
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "FrmSpeak";
+            this.Text = "FrmSpeech";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnSpeak;
+    }
+}
---
+++ XCoder/Tools/FrmSpeak.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XCoder.Tools
+{
+    [DisplayName("语音助手")]
+    public partial class FrmSpeak : Form, IXForm
+    {
+        public FrmSpeak()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSpeak_Click(Object sender, EventArgs e)
+        {
+            var txt = richTextBox1.Text;
+            if (txt.IsNullOrEmpty()) return;
+
+            txt.SpeakAsync();
+        }
+    }
+}
