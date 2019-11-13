b8fc2ab1ab0c72577fb88a466cb1a6c9b8818f58
笑笑 <xxred0324@qq.com>
2019-11-13 23:25:08 +0800

    添加弹窗拓展，修改下拉框设置数据方法

--- XCoderLinux/Util/ControlHelper.cs
+++ XCoderLinux/Util/ControlHelper.cs
@@ -8,6 +8,11 @@ namespace XCoder.Util
 {
     public static class ControlHelper
     {
+        /// <summary>
+        /// 获取当前选择项
+        /// </summary>
+        /// <param name="comboBox"></param>
+        /// <returns></returns>
         public static Object GetActiveObject(this ComboBox comboBox)
         {
             if (comboBox.Active == -1) return null;
@@ -23,9 +28,13 @@ namespace XCoder.Util
             return null;
         }
 
-        public static void AppendValues(this ComboBox comboBox, List<String> values)
+        /// <summary>
+        /// 添加选项
+        /// </summary>
+        /// <param name="comboBox"></param>
+        /// <param name="values"></param>
+        public static void AppendValues<T>(this ComboBox comboBox, List<T> values)
         {
-
             if (!(comboBox.Model is ListStore listStore))
             {
                return;
@@ -35,6 +44,47 @@ namespace XCoder.Util
             {
                 listStore.AppendValues(value);
             }
+        }
+
+        /// <summary>
+        /// 添加选项
+        /// </summary>
+        /// <param name="comboBox"></param>
+        /// <param name="values"></param>
+        public static void SetActive<T>(this ComboBox comboBox, T value)
+        {
+            if (!(comboBox.Model is ListStore listStore))
+            {
+                return;
+            }
+
+            var i = 0;
+            foreach (var item in listStore)
+            {
+                if (value.Equals(item))
+                {
+                    comboBox.Active = i;
+                    return;
+                }
+
+                i++;
+            }
+        }
+
+
+    }
+
+    public static class MessageBox
+    {
+        public static void Show(String text)
+        {
+            if (Window.ListToplevels().Length < 1) return;
+
+            var md = new MessageDialog(Window.ListToplevels()[0],
+                DialogFlags.DestroyWithParent, MessageType.Warning,
+                ButtonsType.Close, text);
+            md.Run();
+            md.Dispose();
         }
     }
 }
