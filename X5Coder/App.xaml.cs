358dc6ace005351862ed9be312cf3a9873e43ee2
大石头 <nnhy@vip.qq.com>
2020-05-05 01:34:52 +0800

    新增新版码神工具X5Coder，WPF风格

---
+++ X5Coder/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using NewLife.Log;
+
+namespace X5Coder
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+#if NET45
+            XTrace.UseWinForm();
+#endif
+
+            base.OnStartup(e);
+        }
+    }
+}
