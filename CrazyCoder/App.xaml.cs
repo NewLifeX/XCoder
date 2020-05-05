c9822279bf6e1518ba0cfb99c186786c0b737396
大石头 <nnhy@vip.qq.com>
2020-05-05 19:04:57 +0800

    疯狂码神

---
+++ CrazyCoder/App.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using NewLife;
+using NewLife.Log;
+using NewLife.Threading;
+using Stardust;
+
+namespace CrazyCoder
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+#if NET45
+            XTrace.UseWinForm();
+#endif
+
+            StartClient();
+
+            StringHelper.EnableSpeechTip = Setting.Current.SpeechTip;
+
+            if (Setting.Current.IsNew) "学无先后达者为师，欢迎使用新生命码神工具！".SpeechTip();
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _Client.Logout("ExitCode=" + e.ApplicationExitCode);
+
+            base.OnExit(e);
+        }
+
+        static TimerX _timer;
+        static StarClient _Client;
+        private static void StartClient()
+        {
+            var set = Setting.Current;
+            var server = set.Server;
+            if (server.IsNullOrEmpty()) return;
+
+            XTrace.WriteLine("初始化服务端地址：{0}", server);
+
+            var client = new StarClient(server)
+            {
+                Code = set.Code,
+                Secret = set.Secret,
+                Log = XTrace.Log,
+            };
+
+            // 登录后保存证书
+            client.OnLogined += (s, e) =>
+            {
+                var inf = client.Info;
+                if (inf != null && !inf.Code.IsNullOrEmpty())
+                {
+                    set.Code = inf.Code;
+                    set.Secret = inf.Secret;
+                    set.Save();
+                }
+            };
+
+            // 可能需要多次尝试
+            _timer = new TimerX(TryConnectServer, client, 0, 5_000) { Async = true };
+
+            _Client = client;
+        }
+
+        private static void TryConnectServer(Object state)
+        {
+            var client = state as StarClient;
+            var set = Setting.Current;
+            client.Login().Wait();
+            CheckUpgrade(client, set.Channel);
+
+            // 登录成功，销毁定时器
+            //TimerX.Current.Period = 0;
+            _timer.TryDispose();
+            _timer = null;
+        }
+
+        private static void CheckUpgrade(StarClient client, String channel)
+        {
+            // 检查更新
+            var ur = client.Upgrade(channel).Result;
+            if (ur != null)
+            {
+                var rs = client.ProcessUpgrade(ur);
+
+                // 强制更新时，马上重启
+                if (rs && ur.Force)
+                {
+                    var p = Process.GetCurrentProcess();
+                    p.Close();
+                    p.Kill();
+                }
+            }
+        }
+    }
+}
---
+++ CrazyCoder/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CrazyCoder
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region 开发工具
+        private void Regex_Click(Object sender, RoutedEventArgs e)
+        {
+            frame.Navigate("Views\\Regex.xaml");
+        }
+
+        private void Ico_Click(Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Security_Click(Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Speech_Click(Object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region 网络通信
+        private void Network_Click(Object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region 帮助
+        private void CheckUpdate_Click(Object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void About_Click(Object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://www.newlifex.com");
+        }
+        #endregion
+    }
+}
