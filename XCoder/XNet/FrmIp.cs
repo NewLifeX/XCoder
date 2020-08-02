519306040a04a07afb003811dda25975872b5e6a
大石头 <nnhy@vip.qq.com>
2020-08-02 12:44:31 +0800

    新增IP设置工具，用于为本机设置多个IP地址

---
+++ XCoder/XNet/FrmIp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NewLife;
+using NewLife.Data;
+using NewLife.Log;
+using NewLife.Net;
+using NewLife.Reflection;
+using NewLife.Threading;
+using NewLife.Windows;
+using XCoder;
+using XCoder.XNet;
+#if !NET4
+using TaskEx = System.Threading.Tasks.Task;
+#endif
+
+namespace XNet
+{
+    [DisplayName("IP设置")]
+    public partial class FrmIp : Form, IXForm
+    {
+        #region 窗体
+        public FrmIp()
+        {
+            InitializeComponent();
+
+            // 动态调节宽度高度，兼容高DPI
+            this.FixDpi();
+
+            Icon = IcoHelper.GetIcon("IP");
+        }
+
+        private void FrmMain_Load(Object sender, EventArgs e)
+        {
+            var ns = NetworkInterface.GetAllNetworkInterfaces();
+            cbAdapter.Tag = ns;
+            cbAdapter.DataSource = ns.Select(e => e.Name).ToArray();
+        }
+        #endregion
+
+        private void cbMode_SelectedIndexChanged(Object sender, EventArgs e)
+        {
+            var name = cbAdapter.SelectedItem + "";
+            if (name.IsNullOrEmpty()) return;
+
+            var ns = cbAdapter.Tag as NetworkInterface[];
+            var ni = ns.FirstOrDefault(e => e.Name == name);
+            if (ni == null) return;
+
+            gbInfo.Tag = ni;
+            gbInfo.Text = ni.Description;
+
+            var ps = ni.GetIPProperties();
+            var ips = ps.UnicastAddresses.Where(e => e.Address.IsIPv4()).ToArray();
+            if (ips.Length == 0) return;
+
+            txtIp.Text = ips[0].Address + "";
+            txtSubMark.Text = ips[0].IPv4Mask + "";
+            txtGateway.Text = ps.GatewayAddresses.Where(e => e.Address.IsIPv4()).Join(",", e => e.Address);
+            txtDns.Text = ps.DnsAddresses.Where(e => e.IsIPv4()).Join();
+
+            var ips2 = ips.Skip(1).OrderBy(e => e.Address.GetAddressBytes().ToLong()).Select(e => e.Address).ToArray();
+            txtIp2.Text = ips2.Join("\r\n");
+        }
+
+        private void btnSet_Click(Object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ XCoder/XNet/FrmIp.designer.cs
@@ -0,0 +1,258 @@
+﻿namespace XNet
+{
+    partial class FrmIp
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbInfo = new System.Windows.Forms.GroupBox();
+            this.txtIp2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDns = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtGateway = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSubMark = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbAdapter = new System.Windows.Forms.ComboBox();
+            this.pnlSetting = new System.Windows.Forms.Panel();
+            this.gbInfo.SuspendLayout();
+            this.pnlSetting.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbInfo
+            // 
+            this.gbInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbInfo.Controls.Add(this.txtIp2);
+            this.gbInfo.Controls.Add(this.label6);
+            this.gbInfo.Controls.Add(this.txtDns);
+            this.gbInfo.Controls.Add(this.label5);
+            this.gbInfo.Controls.Add(this.txtGateway);
+            this.gbInfo.Controls.Add(this.label3);
+            this.gbInfo.Controls.Add(this.txtSubMark);
+            this.gbInfo.Controls.Add(this.label2);
+            this.gbInfo.Controls.Add(this.txtIp);
+            this.gbInfo.Controls.Add(this.btnSet);
+            this.gbInfo.Controls.Add(this.label4);
+            this.gbInfo.Location = new System.Drawing.Point(14, 64);
+            this.gbInfo.Margin = new System.Windows.Forms.Padding(4);
+            this.gbInfo.Name = "gbInfo";
+            this.gbInfo.Padding = new System.Windows.Forms.Padding(4);
+            this.gbInfo.Size = new System.Drawing.Size(858, 491);
+            this.gbInfo.TabIndex = 4;
+            this.gbInfo.TabStop = false;
+            this.gbInfo.Text = "网卡信息";
+            // 
+            // txtIp2
+            // 
+            this.txtIp2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtIp2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtIp2.ForeColor = System.Drawing.Color.BlueViolet;
+            this.txtIp2.Location = new System.Drawing.Point(135, 210);
+            this.txtIp2.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIp2.Multiline = true;
+            this.txtIp2.Name = "txtIp2";
+            this.txtIp2.Size = new System.Drawing.Size(276, 273);
+            this.txtIp2.TabIndex = 41;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 213);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 18);
+            this.label6.TabIndex = 40;
+            this.label6.Text = "辅助IP地址";
+            // 
+            // txtDns
+            // 
+            this.txtDns.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtDns.ForeColor = System.Drawing.Color.BlueViolet;
+            this.txtDns.Location = new System.Drawing.Point(135, 142);
+            this.txtDns.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDns.Name = "txtDns";
+            this.txtDns.Size = new System.Drawing.Size(276, 31);
+            this.txtDns.TabIndex = 39;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 145);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 18);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "DNS";
+            // 
+            // txtGateway
+            // 
+            this.txtGateway.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtGateway.ForeColor = System.Drawing.Color.BlueViolet;
+            this.txtGateway.Location = new System.Drawing.Point(135, 103);
+            this.txtGateway.Margin = new System.Windows.Forms.Padding(4);
+            this.txtGateway.Name = "txtGateway";
+            this.txtGateway.Size = new System.Drawing.Size(276, 31);
+            this.txtGateway.TabIndex = 37;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 106);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "网关";
+            // 
+            // txtSubMark
+            // 
+            this.txtSubMark.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtSubMark.ForeColor = System.Drawing.Color.BlueViolet;
+            this.txtSubMark.Location = new System.Drawing.Point(135, 67);
+            this.txtSubMark.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSubMark.Name = "txtSubMark";
+            this.txtSubMark.Size = new System.Drawing.Size(276, 31);
+            this.txtSubMark.TabIndex = 35;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "子网掩码";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.txtIp.ForeColor = System.Drawing.Color.BlueViolet;
+            this.txtIp.Location = new System.Drawing.Point(135, 31);
+            this.txtIp.Margin = new System.Windows.Forms.Padding(4);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(276, 31);
+            this.txtIp.TabIndex = 33;
+            // 
+            // btnSet
+            // 
+            this.btnSet.Location = new System.Drawing.Point(588, 439);
+            this.btnSet.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(100, 44);
+            this.btnSet.TabIndex = 18;
+            this.btnSet.Text = "打开";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 34);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 18);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "主IP地址";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "网络适配器";
+            // 
+            // cbAdapter
+            // 
+            this.cbAdapter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbAdapter.FormattingEnabled = true;
+            this.cbAdapter.Location = new System.Drawing.Point(135, 10);
+            this.cbAdapter.Margin = new System.Windows.Forms.Padding(4);
+            this.cbAdapter.Name = "cbAdapter";
+            this.cbAdapter.Size = new System.Drawing.Size(363, 26);
+            this.cbAdapter.TabIndex = 9;
+            this.cbAdapter.SelectedIndexChanged += new System.EventHandler(this.cbMode_SelectedIndexChanged);
+            // 
+            // pnlSetting
+            // 
+            this.pnlSetting.Controls.Add(this.label1);
+            this.pnlSetting.Controls.Add(this.cbAdapter);
+            this.pnlSetting.Location = new System.Drawing.Point(14, 12);
+            this.pnlSetting.Margin = new System.Windows.Forms.Padding(4);
+            this.pnlSetting.Name = "pnlSetting";
+            this.pnlSetting.Size = new System.Drawing.Size(858, 46);
+            this.pnlSetting.TabIndex = 13;
+            // 
+            // FrmIp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(880, 568);
+            this.Controls.Add(this.pnlSetting);
+            this.Controls.Add(this.gbInfo);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FrmIp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "网络调试";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.gbInfo.ResumeLayout(false);
+            this.gbInfo.PerformLayout();
+            this.pnlSetting.ResumeLayout(false);
+            this.pnlSetting.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbAdapter;
+        private System.Windows.Forms.Panel pnlSetting;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIp2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDns;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtGateway;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSubMark;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIp;
+    }
+}
