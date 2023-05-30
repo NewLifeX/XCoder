b86b77b68660edd3017479984806835d43682333
智能大石头 <nnhy@vip.qq.com>
2023-05-30 20:58:06 +0800

    增强TTS语音工具，支持选择声音、设置音量和速度

--- XCoder/Tools/FrmSpeak.Designer.cs
+++ XCoder/Tools/FrmSpeak.Designer.cs
@@ -28,47 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.btnSpeak = new System.Windows.Forms.Button();
-            this.SuspendLayout();
+            richTextBox1 = new RichTextBox();
+            btnSpeak = new Button();
+            groupBox1 = new GroupBox();
+            cbVoices = new ComboBox();
+            label3 = new Label();
+            numRate = new NumericUpDown();
+            label2 = new Label();
+            numVolume = new NumericUpDown();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numRate).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numVolume).BeginInit();
+            SuspendLayout();
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBox1.Location = new System.Drawing.Point(12, 13);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(719, 473);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "学无先后达者为师！";
+            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            richTextBox1.Font = new Font("Microsoft YaHei UI", 16F, FontStyle.Regular, GraphicsUnit.Point);
+            richTextBox1.Location = new Point(12, 86);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(997, 485);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "学无先后达者为师！";
             // 
             // btnSpeak
             // 
-            this.btnSpeak.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnSpeak.Location = new System.Drawing.Point(737, 13);
-            this.btnSpeak.Name = "btnSpeak";
-            this.btnSpeak.Size = new System.Drawing.Size(51, 148);
-            this.btnSpeak.TabIndex = 1;
-            this.btnSpeak.Text = "文字转语音";
-            this.btnSpeak.UseVisualStyleBackColor = true;
-            this.btnSpeak.Click += new System.EventHandler(this.btnSpeak_Click);
+            btnSpeak.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnSpeak.Location = new Point(911, 21);
+            btnSpeak.Name = "btnSpeak";
+            btnSpeak.Size = new Size(98, 51);
+            btnSpeak.TabIndex = 1;
+            btnSpeak.Text = "文字转语音";
+            btnSpeak.UseVisualStyleBackColor = true;
+            btnSpeak.Click += btnSpeak_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(cbVoices);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(numRate);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(numVolume);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(794, 68);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "TTS";
+            // 
+            // cbVoices
+            // 
+            cbVoices.DropDownStyle = ComboBoxStyle.DropDownList;
+            cbVoices.FormattingEnabled = true;
+            cbVoices.Location = new Point(66, 26);
+            cbVoices.Name = "cbVoices";
+            cbVoices.Size = new Size(396, 28);
+            cbVoices.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(6, 30);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 20);
+            label3.TabIndex = 4;
+            label3.Text = "声音：";
+            // 
+            // numRate
+            // 
+            numRate.Location = new Point(711, 27);
+            numRate.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            numRate.Minimum = new decimal(new int[] { 10, 0, 0, Int32.MinValue });
+            numRate.Name = "numRate";
+            numRate.Size = new Size(66, 27);
+            numRate.TabIndex = 3;
+            numRate.TextAlign = HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(651, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(54, 20);
+            label2.TabIndex = 2;
+            label2.Text = "速度：";
+            // 
+            // numVolume
+            // 
+            numVolume.Location = new Point(549, 27);
+            numVolume.Name = "numVolume";
+            numVolume.Size = new Size(66, 27);
+            numVolume.TabIndex = 1;
+            numVolume.TextAlign = HorizontalAlignment.Right;
+            numVolume.Value = new decimal(new int[] { 100, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(489, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 20);
+            label1.TabIndex = 0;
+            label1.Text = "音量：";
             // 
             // FrmSpeak
             // 
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(800, 500);
-            this.Controls.Add(this.btnSpeak);
-            this.Controls.Add(this.richTextBox1);
-            this.Name = "FrmSpeak";
-            this.Text = "FrmSpeech";
-            this.ResumeLayout(false);
-
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(1021, 583);
+            Controls.Add(groupBox1);
+            Controls.Add(btnSpeak);
+            Controls.Add(richTextBox1);
+            Name = "FrmSpeak";
+            Text = "FrmSpeech";
+            Load += FrmSpeak_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numRate).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numVolume).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Button btnSpeak;
+        private RichTextBox richTextBox1;
+        private Button btnSpeak;
+        private GroupBox groupBox1;
+        private NumericUpDown numVolume;
+        private Label label1;
+        private NumericUpDown numRate;
+        private Label label2;
+        private ComboBox cbVoices;
+        private Label label3;
     }
 }
--- XCoder/Tools/FrmSpeak.cs
+++ XCoder/Tools/FrmSpeak.cs
@@ -1,29 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.ComponentModel;
+using System.Speech.Synthesis;
 using NewLife;
 
-namespace XCoder.Tools
+namespace XCoder.Tools;
+
+[DisplayName("语音助手")]
+public partial class FrmSpeak : Form, IXForm
 {
-    [DisplayName("语音助手")]
-    public partial class FrmSpeak : Form, IXForm
+    private IList<VoiceInfo> _voices;
+
+    public FrmSpeak()
     {
-        public FrmSpeak()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        private void btnSpeak_Click(Object sender, EventArgs e)
-        {
-            var txt = richTextBox1.Text;
-            if (txt.IsNullOrEmpty()) return;
+    private void FrmSpeak_Load(Object sender, EventArgs e)
+    {
+        var sync = new SpeechSynthesizer();
+        var voices = sync.GetInstalledVoices();
+        //foreach (var vo in voices)
+        //{
+        //    XTrace.WriteLine(vo.VoiceInfo.Name);
+        //}
+        //var dic = voices.ToDictionary(e => e.VoiceInfo.Id, e => e.VoiceInfo.Name);
+        _voices = voices.Select(e => e.VoiceInfo).ToList();
 
-            txt.SpeakAsync();
-        }
+        cbVoices.DataSource = _voices.Select(e => $"{e.Name}[{e.Culture}]").ToList();
+    }
+
+    private void btnSpeak_Click(Object sender, EventArgs e)
+    {
+        var txt = richTextBox1.Text;
+        if (txt.IsNullOrEmpty()) return;
+
+        //txt.SpeakAsync();
+
+        var sync = new SpeechSynthesizer
+        {
+            Volume = (Int32)numVolume.Value,
+            Rate = (Int32)numRate.Value
+        };
+
+        var name = cbVoices.SelectedItem as String;
+        var p = name.IndexOf('[');
+        if (p > 0) name = name[..p];
+        sync.SelectVoice(name);
+        //sync.Voice = _voices.FirstOrDefault(e => e.Name == name);
+
+        sync.SpeakAsync(txt);
     }
 }
