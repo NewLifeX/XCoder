08e06f5380b507ec8caf4d2aa1a8c064a5cb2fdf
大石头 <nnhy@vip.qq.com>
2024-02-15 22:12:56 +0800

    解析雪花Id时，显示时间类型

--- XCoder/Tools/FrmSecurity.cs
+++ XCoder/Tools/FrmSecurity.cs
@@ -505,8 +505,8 @@ public partial class FrmSecurity : Form, IXForm
             SetResult(
                 $"十六：{v:X16}",
                 $"编码：{(t << 2):X8} {workerId:X2} {sequence:X3}",
-                $"基准：{snow.StartTimestamp:yyyy-MM-dd}",
-                $"时间：{time.ToFullString()} ({t} / {(t << 22):X8})",
+                $"基准：{snow.StartTimestamp.ToFullString()} {snow.StartTimestamp.Kind}",
+                $"时间：{time.ToFullString()} {time.Kind} ({t} / {(t << 22):X8})",
                 $"节点：{workerId} ({workerId:X4})",
                 $"序号：{sequence} ({sequence:X4})",
                 $"本机：{snow.WorkerId} ({snow.WorkerId:X4})");
