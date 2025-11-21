7c2a0a7792a83c6fbb40932787dd7925781cad82
Fat Snail <147295383+Fat-Snail@users.noreply.github.com>
2025-11-21 12:14:52 +0800

    feat:新增XCoder-CLI命令行工具

---
+++ XCoder-CLI/CustomBuilderOption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using XCode.Code;
+
+namespace XCode;
+
+public class CustomBuilderOption : EntityBuilderOption
+{
+    /// <summary>
+    /// 是否创建用户自定义业务文件
+    /// </summary>
+    [Description("是否创建用户自定义业务文件")]
+    public Boolean CreateCustomBizFile { get; set; } = false;
+
+    /// <summary>
+    /// 是否在生成.biz.cs文件时覆盖已存在的文件
+    /// </summary>
+    [Description("是否在生成.biz.cs文件时覆盖已存在的文件")]
+    public Boolean OverwriteBizFile { get; set; } = false;
+}
